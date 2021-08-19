16aa34edb053bac4d0960475ef15648e9cb9f81a
Josue Islas / VS CODE <josues.islas01@gmail.com>
2021-08-19 12:06:23 -0500

    Nuevo archivo de fusiones

--- Ejercicios_R/Consola.R
+++
@@ -1,3 +0,0 @@
-#Tener idea de como funciona la instruccion es mejor usar la consola, permite interactuar con otros scrips {sourse}
-bucle <- source(file = "D:/Docs/archivos_r/curso_r/bucle_while.R")
-
--- Ejercicios_R/for.R
+++
@@ -1,8 +0,0 @@
-variable2 <- 0
-
-for (i in 1:5){
-  print(i)
-  variable2 <- i + variable2
-  
-}
-print(variable2)
